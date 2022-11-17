907094206ad09569cf6600eacf8674a7aa3a9921
ThinksSo <stpt.mail@yandex.ru>
2022-11-17 23:16:23 +0300

    Add Lessons 10

---
+++ Lesson10/scripts.js
@@ -0,0 +1,18 @@
+// Task 1
+console.log("I'm program in JavaScript");
+console.log();
+
+
+// Task 2
+let ourPlanetName = "Earth";
+console.log(ourPlanetName);
+const userBookChoice = []; 
+console.log(userBookChoice);
+let currentVisitor = "";
+
+
+// Task 3
+let admin, name;
+name = "John";
+admin = name; // only link copy?
+alert(admin);
